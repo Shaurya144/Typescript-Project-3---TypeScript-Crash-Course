4920384dda34fb5a9925a23fc25b58d332b5f0fe
Shaurya Chundawat <chundawatshaurya@gmail.com>
2024-04-25 20:34:04 +0100

    Changed Variable file

--- 01 Intro/VariableMe.ts
+++ 01 Intro/VariableMe.ts
@@ -1,3 +1,11 @@
 let greeting: string = "Hello Sha"; // this is how you should define variables
 // It stops accidentally assigning different types to the same variable
 console.log(greeting)
+
+// Primitive types - sting, number, boolean
+// Int and Floats are not types, you just use number
+
+let UserId: number = 32452.35
+let LoggedIn: boolean = true
+
+export {}
